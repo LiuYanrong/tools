7af242ce727cb595739301acc73abcc9d6df8562
LiuYanrong <ginlux@foxmail.com>
2020-03-14 12:08:58 +0800

    add bash script of update system application

---
+++ update_app.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#######################################
+# Update system application
+#######################################
+
+die() {
+	echo ERROR: "$@"
+	exit 1
+}
+
+	# update app list
+	sudo apt-get update || die "apt updae Failed!"
+	echo "Update app list ok."
+
+	# download and install app
+	sudo apt-get dist-upgrade || die "download or install Failed!"
+	echo "Update app ok."
+
+	# auto remove not used app
+	sudo apt-get autoremove || die "Auto remove Failed!"
+	echo "Auto remove ok."
+
+	# auto clean
+	sudo apt-get autoclean || die "Auto clean Failed!"
+	echo "Auto clean ok."
+	
