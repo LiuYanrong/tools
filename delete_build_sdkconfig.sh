55d019d1143a722dfb8833a4ffa7ed27d8eff64e
LiuYanrong <ginlux@foxmail.com>
2023-07-03 10:55:38 +0800

    添加删除输入目录下所有子目录下build目录和sdkconfig文件脚本

---
+++ delete_build_sdkconfig.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Function to recursively delete build directories and sdkconfig files
+function delete_build_dirs_and_files() {
+    for file in "$1"/*
+    do
+        if [ -d "$file" ]; then
+            # Delete "build" directories
+            if [ "$(basename "$file")" == "build" ]; then
+                echo "Removing directory: $file"
+                rm -rf "$file"
+            else
+                delete_build_dirs_and_files "$file"
+            fi
+        elif [ -f "$file" ]; then
+            # Delete "sdkconfig" files
+            if [ "$(basename "$file")" == "sdkconfig" ]; then
+                echo "Removing file: $file"
+                rm -f "$file"
+            fi
+        fi
+    done
+}
+
+# Check if input directory is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: ./delete_build_sdkconfig.sh <directory>"
+    exit 1
+fi
+
+# Check if input directory exists
+if [ ! -d "$1" ]; then
+    echo "Invalid directory: $1"
+    exit 1
+fi
+
+# Start from the input directory
+delete_build_dirs_and_files "$1"
+
+echo "Deletion complete."
