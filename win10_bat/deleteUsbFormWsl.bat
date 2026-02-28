3f1cfdbc4583d94bcb2f7e38cdade96432380a4b
LiuYanrong <33168659+LiuYanrong@users.noreply.github.com>
2026-02-28 11:43:22 +0800

    Update deleteUsbFormWsl.bat
    使用 VID:PID 查找设备

--- win10_bat/deleteUsbFormWsl.bat
+++ win10_bat/deleteUsbFormWsl.bat
@@ -3,11 +3,18 @@
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
 
-usbipd list | find "Silicon Labs CP210x USB to UART Bridge" | find "Attached" > usb.tmp
-for /f "delims= " %%i in (usb.tmp) do (usbipd detach --busid %%i  && echo detach %%i)
+for /f "tokens=1" %%i in ('
+    usbipd list ^| find "10c4:ea60" ^| find "Attached"
+') do (
+    usbipd detach --busid %%i && echo detach %%i to wsl
+    usbipd unbind --busid %%i  && echo unbind %%i
+)
 
-usbipd list | find "Silicon Labs CP210x USB to UART Bridge" | find "Shared" > usb.tmp
-for /f "delims= " %%i in (usb.tmp) do (usbipd unbind --busid %%i  && echo unbind %%i)
-del  usb.tmp
+for /f "tokens=1" %%i in ('
+    usbipd list ^| find "10c4:ea60" ^| find "Shared"
+') do (
+    usbipd unbind --busid %%i  && echo unbind %%i
+)
+
 Timeout /T 3 /NOBREAK
 %pause%
