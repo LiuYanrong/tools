b4d7d9b5e1dd9abb301e56b3769569ebefca7aa5
LiuYanrong <33168659+LiuYanrong@users.noreply.github.com>
2026-02-28 11:42:36 +0800

    Update AttachUsbToWsl.bat

--- win10_bat/AttachUsbToWsl.bat
+++ win10_bat/AttachUsbToWsl.bat
@@ -3,11 +3,12 @@
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
 
-usbipd list | find "Silicon Labs CP210x USB to UART Bridge" | find "Not shared" > usb.tmp
-for /f "delims= " %%i in (usb.tmp) do (usbipd bind --force --busid %%i  && echo bind %%i to wsl)
+for /f "tokens=1" %%i in ('
+    usbipd list ^| find "10c4:ea60" ^| find "Not shared"
+') do (
+    usbipd bind --force --busid %%i && echo bind %%i to wsl
+    usbipd attach --wsl --busid %%i  && echo attach %%i to wsl
+)
 
-usbipd list | find "Silicon Labs CP210x USB to UART Bridge" | find "Shared" > usb.tmp
-for /f "delims= " %%i in (usb.tmp) do (usbipd attach --wsl --busid %%i  && echo attach %%i to wsl)
-del  usb.tmp
 Timeout /T 3 /NOBREAK
 %pause%
